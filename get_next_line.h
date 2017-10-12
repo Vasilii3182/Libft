f08997121660087a3c3833bab85864726fc0a442
Olivier FRANCO <ofranco@e3r2p3.42.fr>
2017-10-12 22:37:38 +0200

    ok

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ofranco <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/02/15 15:08:42 by ofranco           #+#    #+#             */
+/*   Updated: 2017/09/21 11:16:21 by ofranco          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <fcntl.h>
+# include <stdlib.h>
+# include "libft.h"
+# define BUFF_SIZE 1
+
+int	get_next_line(const int fd, char **line);
+
+#endif
